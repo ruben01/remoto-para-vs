2695e47136d5345b42e1fcfd3772b45a36542f27
ruben01 <jissiruben@hotmail.com>
2013-12-16 14:01:11 -0400

    sms Twilio prueba #3

--- smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
+++ smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
@@ -13,44 +13,22 @@ namespace smsGitVsSln.Controllers
         //
         // GET: /sms/
 
+        [HttpGet]
         public ActionResult Index()
         {
 
-            string AccountSid = "AAC7329769855ac2319f51129e29352294c";
-            string AuthToken = "30b5abfcedeec6ec14586780e880fc88";
+                return View();
 
-            // instantiate a new Twilio Rest Client
-            var client = new TwilioRestClient(AccountSid, AuthToken);
+        }
+        [HttpPost]
+        public ActionResult Index( string sender,string reciver,string body)
+        {
 
-            // make an associative array of people we know, indexed by phone number
-            var people = new Dictionary<string, string>() {
-                {"+18294072985","viva"},
-                {"+18099282276","Tricom"}
-       
-            };
+            var twilio = new TwilioRestClient("AC7329769855ac2319f51129e29352294c","30b5abfcedeec6ec14586780e880fc88");
+            var sms = twilio.SendSmsMessage(sender,reciver,body);
 
-            // iterate over all our friends
-            foreach (var person in people)
-            {
-                /*
-                // Send a new outgoing SMS by POSTing to the Messages resource 
-                client.SendMessage(
-                    "2766011354", // From number, must be an SMS-enabled Twilio number
-                    person.Key,     // To number, if using Sandbox see note above
-                    // message content
-                    string.Format("Hey {0}, Monkey Party at 6PM. Bring Bananas!", person.Value)
-                );
-
-              */
-
-                client.SendSmsMessage("(276) 601-1354","(809) 928-2276","Esto es una prueba");
-                client.SendSmsMessage("(276) 601-1354", "(829) 407-2985", "Esto es una prueba");
-                client.SendSmsMessage("(276) 601-1354", "(809) 890-6168", "Esto es una prueba");
-
-            }
-
-            return View();
-
+            return Content(sms.Sid);
+            
         }
 
        
