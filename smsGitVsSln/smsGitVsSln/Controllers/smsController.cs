a94beeaef9b71c42c6a094e973520157ea0cfb66
ruben01 <jissiruben@hotmail.com>
2014-05-18 16:13:34 -0400

    update

--- smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
+++ smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
@@ -30,7 +30,7 @@ namespace smsGitVsSln.Controllers
             switch (body)
             {
                 case "ayuda":
-                    respuesta = "1 Nueva Solicitud(ayuda)\n2 Equipos(ayuda)\n3 Formato Fecha";
+                    respuesta = "1 Nueva Solicitud(ayuda)\n2 Equipos(ayuda)\n3 Formato Fecha\n4 Fomato Hora";
                     break;
 
                 case "AYUDA":
@@ -39,17 +39,20 @@ namespace smsGitVsSln.Controllers
 
                 case "1":
 
-                    respuesta = "Formato Solicitud:\n 27/01/2000 09:00-14:00 proyector bocinas laptop";
+                    respuesta = "Formato Solicitud:\n *fecha*horaInicio*horaFin*CodigoEquipo*cantidad*CodigoEquipo2*cantidad";
                     break;
                 case "2":
 
-                    respuesta = "";
+                    respuesta = "Codigos Equipos";//Funcion para devolver todos lo equipos disponibles por modelos
                     break;
                 case "3":
 
-                    respuesta = "";
+                    respuesta = "Dia/Mes/año Ejemplo 24/12/1999";
                     break;
+                case "4":
 
+                    respuesta = "Formato 24H Ejemplo 07:00 ó 20:00 hora fin mayor a la hora inicio";
+                    break;
                 default:
                     respuesta = "No se Reconoce la Instruccion";
                     break;
