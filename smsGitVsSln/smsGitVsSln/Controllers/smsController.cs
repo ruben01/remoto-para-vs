203c863ab3d554fe615c851e7448570b8cd125a0
ruben01 <jissiruben@hotmail.com>
2014-05-17 22:44:07 -0400

    Switch update

--- smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
+++ smsGitVsSln/smsGitVsSln/Controllers/smsController.cs
@@ -30,11 +30,24 @@ namespace smsGitVsSln.Controllers
             switch (body)
             {
                 case "ayuda":
-                    respuesta = "Hola Te brindaremos Ayuda";
+                    respuesta = "1 Nueva Solicitud(ayuda)\n2 Equipos(ayuda)\n3 Formato Fecha";
                     break;
 
                 case "AYUDA":
-                    respuesta = "HOLA MAYUSCULA";
+                    respuesta = "1 Nueva Solicitud(ayuda)\n2 Equipos(ayuda)\n3 Formato Fecha";
+                    break;
+
+                case "1":
+
+                    respuesta = "Formato Solicitud:\n 27/01/2000 09:00-14:00 proyector bocinas laptop";
+                    break;
+                case "2":
+
+                    respuesta = "";
+                    break;
+                case "3":
+
+                    respuesta = "";
                     break;
 
                 default:
